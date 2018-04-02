e6c8118bbe145edfc56e7ab6754686ea9bfe667b
Malemm <malemlink@gmail.com>
2018-04-02 10:46:29 +0530

    version 1.2.3: optimized, code size reduced, and performance improved

--- background.js
+++ background.js
@@ -1,15 +1,13 @@
 /* Background Script */
 
-/* Event listeners defined here */
 chrome.runtime.onMessage.addListener(message_function);
 chrome.tabs.onActivated.addListener(activate_function);
 chrome.tabs.onRemoved.addListener(remove_function);
-chrome.tabs.onUpdated.addListener(update_function);
+chrome.webNavigation.onHistoryStateUpdated.addListener(url_update_function);
 
-
-/* Initialize variables */
-var playing_tab_id = undefined;
-var playing_tab_window_id = undefined;
+var playing_tab_id;
+var playing_tab_window_id;
+var focused_tab_id;
 var map = new Map();
 
 chrome.browserAction.setBadgeBackgroundColor({
@@ -19,47 +17,30 @@ chrome.browserAction.setBadgeText({
     text: "" + map.size
 });
 
-
 function message_function(message, sender, sendResponse) {
-
-    //console.log(message.status);
 
     if (message.status === "played") {
 
-        //console.log("played 0 " + playing_tab_window_id + " " + sender.tab.windowId);
-
         if (playing_tab_window_id !== sender.tab.windowId) {
-
             if (playing_tab_id !== undefined) {
                 chrome.tabs.sendMessage(playing_tab_id, {
                     action: "pause"
                 });
-
             }
-
             playing_tab_id = sender.tab.id;
             playing_tab_window_id = sender.tab.windowId;
-
-            //console.log("played 1 " + playing_tab_window_id);
-
-        } else if (playing_tab_id !== sender.tab.id) {
-
+            
+        } else if (playing_tab_id !== sender.tab.id && focused_tab_id == sender.tab.id) {
             if (playing_tab_id !== undefined) {
                 chrome.tabs.sendMessage(playing_tab_id, {
                     action: "pause"
                 });
-
             }
-
             playing_tab_id = sender.tab.id;
             playing_tab_window_id = sender.tab.windowId;
-
-            //console.log("played 2 " + playing_tab_window_id);
         }
 
-
         if (!map.has(sender.tab.id)) {
-
             map.set(sender.tab.id, sender.tab);
             chrome.browserAction.setBadgeText({
                 text: "" + map.size
@@ -68,10 +49,7 @@ function message_function(message, sender, sendResponse) {
 
     } else if (message.status === "ended") {
 
-        //console.log("2 ended");
-
         if (map.has(sender.tab.id)) {
-
             map.delete(sender.tab.id);
             chrome.browserAction.setBadgeText({
                 text: "" + map.size
@@ -80,36 +58,24 @@ function message_function(message, sender, sendResponse) {
     }
 }
 
-
 function activate_function(info) {
-
     if (map.has(info.tabId) && info.tabId !== playing_tab_id) {
-
         if (playing_tab_id !== undefined) {
             chrome.tabs.sendMessage(playing_tab_id, {
                 action: "pause"
             });
-
         }
-
-        //console.log("3 playing_tab_id: "+playing_tab_id+" info.tabId :"+info.tabId);
-
         playing_tab_id = info.tabId;
         playing_tab_window_id = info.windowId;
-
         chrome.tabs.sendMessage(playing_tab_id, {
             action: "play"
         });
-
     }
-
+    focused_tab_id = info.tabId;
 }
-
 
 function remove_function(tabId, removeInfo) {
-
     if (map.has(tabId)) {
-
         map.delete(tabId);
         chrome.browserAction.setBadgeText({
             text: "" + map.size
@@ -117,21 +83,11 @@ function remove_function(tabId, removeInfo) {
     }
 }
 
-
-function update_function(tabId, changeInfo, tab) {
-
-    if (map.has(tabId) && changeInfo.url !== undefined) {
-
-        map.delete(tabId);
+function url_update_function(details) {
+    if (map.has(details.tabId)) {
+        map.delete(details.tabId);
         chrome.browserAction.setBadgeText({
             text: "" + map.size
         });
-
-
-        chrome.tabs.sendMessage(tabId, {
-            action: "update_var"
-        });
-
     }
 }
-
--- content.js
+++ content.js
@@ -1,106 +1,40 @@
 /* Content Script */
 
-var play_message_sent = false;
+chrome.runtime.onMessage.addListener(message_callback);
 
-
-// Getting the video object
 var video = document.querySelector("video");
 
+function onplay_callback() {
+    chrome.runtime.sendMessage({
+        status: "played"
+    });
+}
 
-// This function will check for update of video selector, especially for vimeo
+function onended_callback() {
+    chrome.runtime.sendMessage({
+        status: "ended"
+    });
+
+}
+
+// This function will wait for the video element to come
 var observer = setInterval(observe_function, 1000);
 
 function observe_function() {
-
     if (video) {
-
-        // Send message to the background when the video starts to play
-        video.onplay = function () {
-
-            // This condition will be false for the first time play from begining; this is absolutely necessary! (^_^) 
-            if (video.currentTime >= 0.3) {
-
-                console.log("onplay "+video.currentTime);
-
-                chrome.runtime.sendMessage({
-                    status: "played"
-                });
-
-                play_message_sent = true;
-
-            }
-        };
-
-
-        video.onended = function () {
-
-            //console.log("onended");
-
-            chrome.runtime.sendMessage({
-                status: "ended"
-            });
-
-            play_message_sent = false;
-        };
-
-
-        // onplay only is not enough for dynamic site like youtube! This is backup incase onplay failed to send the message
-        video.ontimeupdate = function () {
-
-            // this condition is put to stop youtube from firing when tab is opened in new tab without focusing, DOESN'T WORK ON VIMEO !!!
-
-            if (!play_message_sent && video.currentTime >= 0.3) {
-
-                console.log("ontimeupdate "+video.currentTime);
-
-                chrome.runtime.sendMessage({
-                    status: "played"
-                });
-
-                play_message_sent = true;
-            }
-
-        }
-
-        video.onabort = function () {
-
-            play_message_sent = false;
-
-            //console.log("onabort");
-        }
-
+        video.addEventListener("play", onplay_callback);
+        video.addEventListener("ended", onended_callback);
         clearInterval(observer);
-        //console.log("hodor: i've got it");
-
     } else {
-
         video = document.querySelector("video");
-        //console.log("i am groot: still looking for it");
     }
 }
 
-
-// Set up message listener
-chrome.runtime.onMessage.addListener(action_function);
-
-// Callback function 
-function action_function(message, sender, sendResponse) {
-
-    //console.log("Received action: " + message.action);
+function message_callback(message, sender, sendResponse) {
     if (message.action === "pause") {
-
         video.pause();
-
     } else if (message.action === "play") {
-
-        // Rewind the video 1 second
-        video.currentTime = video.currentTime - 1;
+        video.currentTime = video.currentTime - 0.5;
         video.play();
-
-    } else if (message.action === "update_var") {
-        
-        video.currentTime = 0;
-        video.pause();
-        play_message_sent = false;
     }
 }
